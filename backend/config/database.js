cb48323ece83fc15864f3a2e5417fe93eb00079c
Haris Ali Khan <iitianharis@gmail.com>
2022-05-22 10:57:26 +0530

    Video 10 c

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,6 +9,7 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", "harry"),
       schema: env("DATABASE_SCHEMA", "public"),
+      ssl: { rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), },
     },
   }
 });
